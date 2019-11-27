e31edebfb629a38184384a0ac798381bf70e9135
Alejandro Sanchez <alejandrogsanchez18@gmail.com>
2019-11-27 10:03:49 -0300

    send request

--- src/ChatApiDriver.php
+++ src/ChatApiDriver.php
@@ -170,6 +170,7 @@ class ChatApiDriver extends HttpDriver
      */
     public function sendRequest($endpoint, array $parameters, IncomingMessage $matchingMessage)
     {
+        $this->config = Collection::make($this->getConfiguration($parameters['instance']));
         $url = $this->config->get('instance_url') . "/{$endpoint}?token={$this->config->get('token')}";
         $this->http->post($url, [], $parameters);
     }
