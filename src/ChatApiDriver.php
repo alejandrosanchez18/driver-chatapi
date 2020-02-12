4bbc8caac3ca24756e15e2c5a86894a6bdc4c8da
Alejandro Sanchez <alejandrogsanchez18@gmail.com>
2020-02-12 17:00:42 -0300

    cambiar device por instance

--- src/ChatApiDriver.php
+++ src/ChatApiDriver.php
@@ -159,7 +159,7 @@ class ChatApiDriver extends HttpDriver
      */
     public function getConfiguration($instanceId) : array
     {
-        $device = \App\Device::where('uid', $instanceId)->pluck('token')->first();
+        $instance = \App\Instance::where('uid', $instanceId)->pluck('token')->first();
         return [
                     'instance_url' => "https://api.chat-api.com/instance{$instanceId}/",
                      'token' => $device
