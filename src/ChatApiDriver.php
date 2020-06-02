6538e908e8709003eedff514e9a8524f1e2da685
Alejandro Sanchez <alejandrogsanchez18@gmail.com>
2020-06-02 09:30:58 -0300

    agregar condicion para buscar instance ID

--- src/ChatApiDriver.php
+++ src/ChatApiDriver.php
@@ -163,11 +163,15 @@ class ChatApiDriver extends HttpDriver
      */
     public function getConfiguration($instanceId) : array
     {
-        $instance = \App\Instance::where('uid', $instanceId)->pluck('token')->first();
-        return [
-                    'instance_url' => "https://api.chat-api.com/instance{$instanceId}/",
-                     'token' => $instance
-            ];
+        if(instanceId){
+            $instance = \App\Instance::where('uid', $instanceId)->pluck('token')->first();
+            return [
+                        'instance_url' => "https://api.chat-api.com/instance{$instanceId}/",
+                         'token' => $instance
+                ];
+        } else {
+            return '';
+        }
     }
 
 
